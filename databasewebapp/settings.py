70dd8b3027a940de2f0c516539734d5b1c6804cd
Moshii <kunnako_1@hotmail.com>
2020-02-26 21:39:59 +0700

    deploy

--- databasewebapp/settings.py
+++ databasewebapp/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+django_heroku.settings(locals())
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
+    
 ]
 
 MIDDLEWARE = [
